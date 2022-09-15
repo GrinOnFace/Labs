5cb43f352bda2cf89f871230ac2453a248b6d692
Renat <renatnabiew@inbox.ru>
2022-09-16 00:17:47 +0300

    Лабы

--- Лаба 2/Лаба 2.cpp
+++ Лаба 2/Лаба 2.cpp
@@ -1,10 +1,30 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <cmath>
 #include <stdio.h>
+
 int main() {
-	printf("please enter a value");
-	float x;
-	
 	return 0;
 }
-int itteration()
+
+void insert(double* x, double* y, double* iterrations) {
+	printf("input range:\n");
+	scanf("%lf %lf", x, y);
+	printf("How much iterrations?\n");
+	scanf("%lf", iterrations);
+}
+
+double change(double x, double y, double itterations) {
+	double change;
+	change = ((x - y) / itterations - 1);
+	return change;
+}
+bool error(double x, double y, double itterations) {
+	if (x < y) {
+		printf("Error. Try again\n");
+		return false;
+	}
+	else if (x == y && itterations != 1) {
+		printf("Error. Try again\n");
+		return false;
+	}
+}
