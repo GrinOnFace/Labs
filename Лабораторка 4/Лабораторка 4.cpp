fc9ea70bfbdb63594014e52e1902798335cc16de
Renat <renatnabiew@inbox.ru>
2022-10-21 02:06:07 +0300

    'Лабы'

--- Лабораторка 4/Лабораторка 4.cpp
+++ Лабораторка 4/Лабораторка 4.cpp
@@ -3,135 +3,113 @@
 #include <malloc.h>
 
 int userInputArraySize();
-void CleanUp(double* arr);
-int userInputArray(double* arr, int arrSize);
 int calculation(double* arr, int arrSize, double* sum);
-void MallocF(double** arr, int arrSize);
-void mediana(double max, double min, unsigned int maxPosition, int arrSize, double* sum, double* arr);
-int check(unsigned int maxPosition, int arrSize);
+int mallocF(double** arr, int arrSize);
+int medianaF(double max, double min, unsigned int maxPosition, int arrSize, double* sum, double* arr);
 
-int main()
+void main()
 {
-	int arrSize = 0;
-	double* arr = NULL, sum = 0;
-	if (2 >= (arrSize = userInputArraySize()))
-	{
-		printf("Error. Incorrect array size\n");
-		CleanUp(arr);
-	}
-	else
-	{
-		MallocF(&arr, arrSize);
-		if (arr)
-		{
-			CleanUp(arr);
-		}
-		if (0 != userInputArray(arr, arrSize))
-		{
-			printf("Input failed\n");
-			CleanUp(arr);
-		}
-		if (0 != calculation(arr, arrSize, &sum))
-		{
-			printf("Input failed\n");
-			CleanUp(arr);
-		}
-		printf("result is %lf\n", sum);
-	}
+    int arrSize = 0;
+    double sum = 0;
+    if (2 >= (arrSize = userInputArraySize()))
+    {
+        printf("Error. Incorrect array size\n");
+    }
+    else
+    {
+        double* arr = NULL;
+        if (mallocF(&arr, arrSize) == 0)
+        {
+            printf("Memory not allocated");
+        }
+        else
+        {
+            if (calculation(arr, arrSize, &sum) == 0)
+            {
+                printf("result is %lf\n", sum);
+                free(arr);
+            }
+            else {
+                free(arr);
+            }
+        }
+    }
 }
 
-void MallocF(double** arr, int arrSize)
+int mallocF(double** arr, int arrSize)
 {
-	double* temp = (double*)malloc(arrSize * sizeof(double));
-	*arr = temp;
-}
-
-void CleanUp(double* arr)
-{
-	if (arr)
-	{
-		free(arr);
-	}
+    double* temp = (double*)malloc(arrSize * sizeof(double));
+    *arr = temp;
+    unsigned int ans;
+    printf("Input elements of array:\n");
+    if (temp != NULL) 
+    {
+        for (int i = 0; i < arrSize; ++i)
+        {
+            scanf("%lf", temp + i);
+        }
+        ans = 1;
+    }
+    else {
+        ans = 0;
+    }
+    return ans;
 }
 
 int userInputArraySize()
 {
-	printf("Input Array Size\n");
-	int size = 0;
-	if (1 != scanf("%d", &size))
-	{
-		return 0;
-	}
-	return size;
-}
-
-int userInputArray(double* arr, int arrSize)
-{
-	int counter = 0;
-	printf("Input %d numbers\n", arrSize);
-	while (counter < arrSize)
-	{
-		if (1 != scanf("%lf", arr + counter++))
-		{
-			return -1;
-		}
-	}
-	return 0;
-}
-
-void mediana(double max, double min, unsigned int maxPosition, int arrSize, double* sum, double* arr)
-{
-	double mediana = (min + max) / 2;
-	for (int i = maxPosition + 1; i < arrSize; i++)
-	{
-		if (*(arr + i) > mediana)
-		{
-			*sum += *(arr + i);
-		}
-	}
-}
-
-int check(unsigned int maxPosition, int arrSize)
-{
-	if ((maxPosition + 1 == arrSize) || (maxPosition + 2 == arrSize))
-	{
-		printf("No such elements\n");
-		return -1;
-	}
-	return 0;
+    printf("Input Array Size\n");
+    int size = 0;
+    if (1 != scanf("%d", &size))
+    {
+        return 0;
+    }
+    return size;
 }
 
 int calculation(double* arr, int arrSize, double* sum)
 {
-	double max = *arr;
-	unsigned int maxPosition = 0;
-	for (int i = 0; i < arrSize; i++)
-	{
-		if (*(arr + i) > max)
-		{
-			max = *(arr + i);
-			maxPosition = i;
-		}
-	}
-	if (-1 == check(maxPosition, arrSize))
-	{
-		return -1;
-	}
-	else {
-		double min = max;
-		max = *(arr + maxPosition + 1);
-		for (int i = maxPosition + 1; i < arrSize; i++)
-		{
-			if (*(arr + i) > max)
-			{
-				max = *(arr + i);
-			}
-			if (*(arr + i) < min)
-			{
-				min = *(arr + i);
-			}
-		}
-		mediana(max, min, maxPosition, arrSize, sum, arr);
-		return 0;
-	}
+    int ans = 0;
+    double max = *arr;
+    double min = *arr;
+    unsigned int maxPosition = 0;
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (*(arr + i) > max)
+        {
+            max = *(arr + i);
+            maxPosition = i;
+        }
+    }
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (*(arr + i) < min)
+        {
+            min = *(arr + i);
+        }
+    }
+    if (medianaF(max, min, maxPosition, arrSize, sum, arr) == 1)
+    {
+        printf("No such elements\n");
+        ans = 1;
+    }
+    return ans;
+}
+
+int medianaF(double max, double min, unsigned int maxPosition, int arrSize, double* sum, double* arr)
+{
+    int ans = 0;
+    unsigned int counter = 0;
+    double middle = ((max + min) / 2);
+    for (int i = maxPosition + 1; i < arrSize; i++)
+        if (*(arr + i) > middle)
+        {
+            *sum += *(arr + i);
+            counter++;
+        }
+    if (counter == 0)
+    {
+        ans = 1;
+    }
+    return ans;
 }
---
+++ Лабораторка 5/Лабораторка 5.cpp
@@ -0,0 +1,114 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+
+void userInputArraySize(int* arrSize);
+int mallocF(double** arr, int arrSize);
+
+void swap(double* pa, double* pb);
+int check(int arrSize);
+
+void sort(double* arr, int arrSize);
+int groupElem(double* arr, int arrSize);
+void sortBelowZero(double* arr, int arrSize);
+void resArray(double* arr, int arrSize);
+
+void main()
+{
+    int arrSize = 0;
+    userInputArraySize(&arrSize);
+    if(check(arrSize) == 1)
+    {
+        double* arr = NULL;
+        if (mallocF(&arr, arrSize) == 0)
+        {
+            printf("Memory not allocated");
+        }
+        else
+        {
+            sort(arr, arrSize);
+            groupElem(arr, arrSize);
+            sortBelowZero(arr, arrSize);
+            printf("Result of the sorted is: \n");
+            resArray(arr, arrSize);
+            free(arr);
+        }
+    }
+    else
+    {
+        printf("Size should be greater than zero");
+    }
+}
+
+int mallocF(double** arr, int arrSize)
+{
+    double* temp = (double*)malloc(arrSize * sizeof(double));
+    *arr = temp;
+    unsigned int ans;
+    printf("Input elements of array:\n");
+    if (temp != NULL) {
+        for (int i = 0; i < arrSize; ++i) {
+            scanf("%lf", temp + i);
+        }
+        ans = 1;
+    }
+    else {
+        ans = 0;
+    }
+    return ans;
+}
+
+int check(int arrSize) 
+{
+    unsigned int ans;
+    if (arrSize < 0)
+        ans = 0;
+    else
+        ans = 1;
+    return ans;
+}
+
+void swap(double* pa, double* pb)
+{
+    double temp = *pa;
+    *pa = *pb;
+    *pb = temp;
+}
+
+void userInputArraySize(int* arrSize)
+{
+    printf("Input Array Size\n");
+    scanf("%d", arrSize);
+}
+
+void sort(double* arr, int arrSize)
+{
+    for (int i = 0; i < arrSize - 1; i++)
+        for (int j = i + 1; j < arrSize; j++)
+            if (*(arr + i) > *(arr + j))
+                swap(arr + i, arr + j);
+}
+
+int groupElem(double* arr, int arrSize)
+{
+    int j = 0;
+    for (int i = 0; i < arrSize; i++)
+        if (*(arr + i) >= 0) {
+            swap(arr + i, arr + j);
+            j++;
+        }
+    return j;
+}
+
+void sortBelowZero(double* arr, int arrSize) {
+    for (int i = 0; i < arrSize - 1; i++)
+        for (int j = i + 1; j < arrSize; j++)
+            if (*(arr + i) < 0 && *(arr + i) > *(arr + j))
+                swap(arr + i, arr + j);
+}
+
+void resArray(double* arr, int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        printf("%lf\n", *(arr + i));
+    }
+}
